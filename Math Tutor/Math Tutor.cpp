dc5c76c7934df01c2707be922294f08083ce7d82
harmo <harmoniross123@gmail.com>
2025-10-15 18:27:03 -0400

    The updated code

--- Math Tutor/Math Tutor.cpp
+++ Math Tutor/Math Tutor.cpp
@@ -1,7 +1,37 @@
-# Include <Iosstream>
-# Include <iomanip>
-unsing namespace std;
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
 int main() {
+    srand(time(0)); // random number generator
+    char again = 'Y';
 
+    cout << "Math Tutor\n\n";
+
+    while (again == 'Y' || again == 'y') {
+        // Generate two random numbers
+        int num1 = rand() % 10 + 1;
+        int num2 = rand() % 10 + 1;
+        int correctAnswer = num1 + num2;
+        int userAnswer;
+
+        // Ask the question
+        cout << "What is " << num1 << " + " << num2 << "? ";
+        cin >> userAnswer;
+
+        // Check answer
+        if (userAnswer == correctAnswer)
+            cout << "Correct!\n";
+        else
+            cout << "Sorry, the correct answer is " << correctAnswer << ".\n";
+
+        // Ask to continue
+        cout << "Try another problem? (Y/N): ";
+        cin >> again;
+        cout << endl;
+    }
+
+    cout << "Goodbye!\n";
+    return 0;
 }
