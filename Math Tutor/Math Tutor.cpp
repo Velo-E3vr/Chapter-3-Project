1973e6efa8b49c0b066c7c1bb3e853d018b80523
harmo <harmoniross123@gmail.com>
2025-10-16 18:14:17 -0400

    Finalized project included

--- Math Tutor/Math Tutor.cpp
+++ Math Tutor/Math Tutor.cpp
@@ -16,22 +16,4 @@ int main() {
         int correctAnswer = num1 + num2;
         int userAnswer;
 
-        // Ask the question
-        cout << "What is " << num1 << " + " << num2 << "? ";
-        cin >> userAnswer;
-
-        // Check answer
-        if (userAnswer == correctAnswer)
-            cout << "Correct!\n";
-        else
-            cout << "Sorry, the correct answer is " << correctAnswer << ".\n";
-
-        // Ask to continue
-        cout << "Try another problem? (Y/N): ";
-        cin >> again;
-        cout << endl;
-    }
-
-    cout << "Goodbye!\n";
-    return 0;
 }
