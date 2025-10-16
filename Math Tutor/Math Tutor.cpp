521a1bbb560202c84cf46f4077f269c98bfab432
Velo-E3vr <harmoniross123@gmail.com>
2025-10-16 18:21:44 -0400

    Enhance math tutor with question asking and feedback
    Added user interaction for answering math questions and checking correctness.

--- Math Tutor/Math Tutor.cpp
+++ Math Tutor/Math Tutor.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    srand(time(0)); // random number generator
+    srand(time(0)); // Seed random number generator
     char again = 'Y';
 
     cout << "Math Tutor\n\n";
@@ -16,4 +16,22 @@ int main() {
         int correctAnswer = num1 + num2;
         int userAnswer;
 
+        // Ask the question
+        cout << "What is " << num1 << " + " << num2 << "? ";
+        cin >> userAnswer;
+
+        // Check answer
+        if (userAnswer == correctAnswer)
+            cout << "Correct!\n";
+        else
+            cout << "Sorry, the correct answer is " << correctAnswer << ".\n";
+
+        // Ask to continue
+        cout << "Try another problem? (Y/N): ";
+        cin >> again;
+        cout << endl;
+    }
+
+    cout << "Goodbye!\n";
+    return 0;
 }
